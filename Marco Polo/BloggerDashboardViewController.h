507c3496944cef1851f2e2009ca489a8f3a5cca4
Roland Tecson <roland_tecson@hotmail.com>
2013-03-23 15:01:38 -0700

    Implement

---
+++ Marco Polo/BloggerDashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  BloggerDashboardViewController.h
+//  Marco Polo
+//
+//  Created by Roland on 2013-03-23.
+//  Copyright (c) 2013 com.marcopolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BloggerDashboardViewController : UIViewController
+
+@end
---
+++ Marco Polo/BloggerDashboardViewController.m
@@ -0,0 +1,38 @@
+//
+//  BloggerDashboardViewController.m
+//  Marco Polo
+//
+//  Created by Roland on 2013-03-23.
+//  Copyright (c) 2013 com.marcopolo. All rights reserved.
+//
+
+#import "BloggerDashboardViewController.h"
+
+@interface BloggerDashboardViewController ()
+
+@end
+
+@implementation BloggerDashboardViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
