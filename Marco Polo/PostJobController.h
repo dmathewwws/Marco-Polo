42cbea70e1d321a2bf232f5fbbce244a339fc42f
dannylm <dannyl.mathews@gmail.com>
2013-03-24 14:16:44 -0700

    Implement

--- Marco Polo/PostJobController.h
+++ Marco Polo/PostJobController.h
@@ -17,11 +17,21 @@
 @property (strong, nonatomic) IBOutlet UITextField *category;
 @property (nonatomic, strong) NSArray *categories;
 @property (nonatomic) NSInteger selectedIndex;
+@property (nonatomic, strong) NSArray *blogsPerWeekArray;
+@property (nonatomic) NSInteger selectedIndexBlogsPerWeek;
+@property (nonatomic, strong) NSArray *durationArray;
+@property (nonatomic) NSInteger selectedIndexDuration;
 
 @property (strong, nonatomic) ActionSheetStringPicker *stringPicker;
+@property (strong, nonatomic) ActionSheetStringPicker *blogsPerWeekPicker;
+@property (strong, nonatomic) ActionSheetStringPicker *durationPicker;
 
 - (IBAction)submitJob:(id)sender;
 - (void) dismissKeyboard:(id) sender;
 - (IBAction)pickCategory:(id)sender;
 
+- (IBAction)pickBlogsPerWeek:(id)sender;
+- (IBAction)pickDuration:(id)sender;
+
+- (void)animalWasSelected:(NSNumber *)selectedIndex element:(id)element;
 @end
--- Marco Polo/PostJobController.m
+++ Marco Polo/PostJobController.m
@@ -25,6 +25,12 @@
 @synthesize categories = _categories;
 @synthesize selectedIndex = _selectedIndex;
 @synthesize category = _category;
+@synthesize selectedIndexBlogsPerWeek = _selectedIndexBlogsPerWeek;
+@synthesize selectedIndexDuration = _selectedIndexDuration;
+@synthesize blogsPerWeekPicker = _blogsPerWeekPicker;
+@synthesize durationPicker = _durationPicker;
+@synthesize blogsPerWeekArray = _blogsPerWeekArray;
+@synthesize durationArray = _durationArray;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -68,11 +74,16 @@
     _durationPerWeek.delegate = self;
     _dollarsPerWeek.delegate = self;
     _category.delegate = self;
+    _jobDescriptionTextView.delegate = self;
+    
     
     self.categories = [NSArray arrayWithObjects:@"Automotive", @"Technology", @"Fashion", nil];
+    self.blogsPerWeekArray = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", nil];
+    self.durationArray = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"11", @"12", nil];
     
     [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard:)]];
-
+    
+    NSLog(@"Made it to end of ViewDIDLoad");
 
 }
 
@@ -132,16 +143,53 @@
 
 - (IBAction)pickCategory:(id)sender {
     
-    [ActionSheetStringPicker showPickerWithTitle:@"Select Category" rows:self.categories initialSelection:self.selectedIndex target:self successAction:@selector(animalWasSelected:element:) cancelAction:@selector(actionPickerCancelled:) origin:sender];
+    _stringPicker = [ActionSheetStringPicker showPickerWithTitle:@"Select Category" rows:self.categories initialSelection:self.selectedIndex target:self successAction:@selector(animalWasSelected:element:) cancelAction:@selector(actionPickerCancelled:) origin:sender];
 
 }
 
 - (void)animalWasSelected:(NSNumber *)selectedIndex element:(id)element {
-    self.selectedIndex = [selectedIndex intValue];
     
-    //may have originated from textField or barButtonItem, use an IBOutlet instead of element
-    self.category.text = [self.categories objectAtIndex:self.selectedIndex];
-    [_category resignFirstResponder];
+    NSLog(@"in animalWasSelected element is %@",element);
+    
+    if (element == _category) {
+        self.selectedIndex = [selectedIndex intValue];
+        
+        //may have originated from textField or barButtonItem, use an IBOutlet instead of element
+        self.category.text = [self.categories objectAtIndex:self.selectedIndex];
+        [_category resignFirstResponder];
+        
+            NSLog(@"in animalWasSelected elemeent = _stringPicker ");
+        
+    }else if (element == _blogsPerWeek) {
+            self.selectedIndexBlogsPerWeek = [selectedIndex intValue];
+            
+            //may have originated from textField or barButtonItem, use an IBOutlet instead of element
+            self.blogsPerWeek.text = [self.blogsPerWeekArray objectAtIndex:self.selectedIndexBlogsPerWeek];
+            [_blogsPerWeek resignFirstResponder];
+        
+                    NSLog(@"in animalWasSelected elemeent = _blogsPerWeekPicker ");
+    }else if (element == _durationPerWeek) {
+        self.selectedIndexDuration = [selectedIndex intValue];
+        
+        //may have originated from textField or barButtonItem, use an IBOutlet instead of element
+        self.durationPerWeek.text = [self.durationArray objectAtIndex:self.selectedIndexDuration];
+        [_durationPerWeek resignFirstResponder];
+        
+                            NSLog(@"in animalWasSelected elemeent = _durationPerWeek ");
+    }
+}
+
+
+- (IBAction)pickBlogsPerWeek:(id)sender {
+    
+    [ActionSheetStringPicker showPickerWithTitle:@"Select Option" rows:self.blogsPerWeekArray initialSelection:self.selectedIndexBlogsPerWeek target:self successAction:@selector(animalWasSelected:element:) cancelAction:@selector(actionPickerCancelled:) origin:sender];
+    
+}
+
+- (IBAction)pickDuration:(id)sender {
+    
+    [ActionSheetStringPicker showPickerWithTitle:@"Select Option" rows:self.durationArray initialSelection:self.selectedIndexDuration target:self successAction:@selector(animalWasSelected:element:) cancelAction:@selector(actionPickerCancelled:) origin:sender];
+    
 }
 
 - (void)actionPickerCancelled:(id)parm
@@ -159,6 +207,10 @@
 
 -(void)textFieldDidBeginEditing:(UITextField *)textField{
     
+    if(textField != _dollarsPerWeek ){
+        [textField resignFirstResponder];
+    }
+    
 //    [self animateTextField:textField up:YES];
     
 }
@@ -166,12 +218,31 @@
 - (void)textFieldDidEndEditing:(UITextField *)textField{
 //    [self animateTextField:textField up:NO];
     
+    if(textField == _dollarsPerWeek ){
+        NSLog(@"total value is: %d",((self.selectedIndexBlogsPerWeek)+1)*((self.selectedIndexDuration)+1)*[_dollarsPerWeek.text intValue]);
+        NSLog(@"self.selectedIndexBlogsPerWeek is: %d",((self.selectedIndexBlogsPerWeek)+1));
+        NSLog(@"self.selectedIndexDuration is: %d",((self.selectedIndexDuration)+1));
+        NSLog(@"_dollarsPerWeek.text intValue is: %d",[_dollarsPerWeek.text intValue]);
+        
+    }
+    
 }
 
+-(void)textViewDidBeginEditing:(UITextView *)textView{
+    
+    [self animateTextField:textView up:YES];
+
+}
+
+-(void)textViewDidEndEditing:(UITextView *)textView{
+    
+    [self animateTextField:textView up:NO];
+    [_jobDescriptionTextView resignFirstResponder];
+}
 
 -(void)animateTextField:(id)textField up:(BOOL)up
 {
-    const int movementDistance = -135; // tweak as needed
+    const int movementDistance = -195; // tweak as needed
     const float movementDuration = 0.3f; // tweak as needed
     
     int movement = (up ? movementDistance : -movementDistance);
