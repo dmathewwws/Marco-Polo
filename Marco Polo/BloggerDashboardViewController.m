cbab62e774fa7c19276e0eec37db995b067af8ba
Roland Tecson <roland_tecson@hotmail.com>
2013-03-23 17:02:02 -0700

    Implementation

--- Marco Polo/BloggerDashboardViewController.m
+++ Marco Polo/BloggerDashboardViewController.m
@@ -8,11 +8,15 @@
 
 #import "BloggerDashboardViewController.h"
 
-@interface BloggerDashboardViewController ()
+@interface BloggerDashboardViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *bloggerDashboardTableView;
 
 @end
 
+
 @implementation BloggerDashboardViewController
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -23,16 +27,57 @@
     return self;
 }
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    self.bloggerDashboardTableView.delegate = self;
+    self.bloggerDashboardTableView.dataSource = self;
 }
+
+
+- (void)viewDidUnload
+{
+    [self setBloggerDashboardTableView:nil];
+    [self setBloggerDashboardTableView:nil];
+    [super viewDidUnload];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark UITableViewDataSource methods
+
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section
+{
+    NSLog(@"In numberOfRowsInSection, for section %d", section);
+    return 2;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *cellIdentifier = [NSString stringWithFormat:@"s%ir%i", indexPath.section, indexPath.row];
+    NSLog(@"Using cellID = %@", cellIdentifier);
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:cellIdentifier];
+        //you can customize your cell here because it will be used just for one row.
+    }
+    
+    return cell;
+}
+
 
 @end
